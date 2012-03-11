6cb3abbe3c1c0c5628cc8437b411bbcfe0924015
Neo futur <neofutur@ww7.pe>
2012-03-11 11:09:24 +0000

    still fixing things

--- out.php
+++ out.php
@@ -1,7 +1,7 @@
 <?php
  require_once 'header.php';
 
- $id_site=mysql_real_escape_string($_GET['site']);
+ if ( isset ( $_GET['site'] ) ) $id_site=mysql_real_escape_string($_GET['site']); else $id_site="";
 
  if ( $id_site )
  {
